ce5b747ca97b9389d0a3dde4c5638ac69863ebc9
root <pisdak79@gmail.com>
2017-05-29 14:39:54 +0530

    Its working awesome!

--- signup/urls.py
+++ signup/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.register, name='index'),
     url(r'^register/', views.register, name='index'),
-    url(r'^thanks/', views.thanks, name='thanks'),
     url(r'^verify/', views.verify, name='verify'),
+    url(r'^enterotp/', views.enterotp, name='enterotp'),
 ]
--- signup/views.py
+++ signup/views.py
@@ -3,8 +3,9 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .forms import RegisterForm, PartialRegisterForm
 from .models import User
 import random
-from django.core.mail import send_mail, BadHeaderError
+from django.core.mail import send_mail, BadHeaderError, EmailMultiAlternatives
 from django.core import mail
+from django.template.loader import render_to_string	
 
 connection = mail.get_connection()
 
@@ -19,8 +20,10 @@ def register(request):
 			connection.open()
 			finalForm = partialForm.save(commit = False)
 			finalForm.key = random.randint(9999,100000)
-			message = finalForm.key
-			subject = 'ThankYou'
+			# url = '127.0.0.1:8000/signup/verify/?uniquekey='+
+			context = {'otp' : finalForm.key, 'uniqueKey':finalForm.uniqueKey, 'email':finalForm.email}
+			message = render_to_string('email.html', context = context).strip()
+			subject = 'Confirm your account'
 			finalForm.save();
 			fromMail = 'pisdak79@gmail.com'
 			toMail = request.POST.get('email','')
@@ -30,26 +33,42 @@ def register(request):
 				connection.close()
 				return HttpResponse('Invalid header found.')
 			connection.close()
-			return HttpResponseRedirect('/signup/verify/')
+			redirect_url = '/signup/enterotp/?email='+finalForm.email+'&&uniqueKey='+str(finalForm.uniqueKey)
+			return HttpResponseRedirect(redirect_url)
 	else:
 		form = PartialRegisterForm()
 
 	return render(request, 'signup.html', {'form' : form})
 
-def thanks(request):
-	return HttpResponse('Thank You!')
-
 def verify(request):
-	req_uniqueKey = request.GET.get('uniqueKey','')
-	req_otp = request.GET.get('otp', '')
-	req_email = request.GET.get('email','')
-	db_obj = User.objects.get(email=req_email)
-	if db_obj.uniqueKey == req_uniqueKey:
-		if db_obj.otp == req_otp:
-			message = 'I found you!! :)'
+	if request.method == 'GET':
+		req_uniqueKey = request.GET.get('uniqueKey','')
+		req_otp = request.GET.get('otp', '')
+		req_email = request.GET.get('email','')
+		db_obj = User.objects.get(email=req_email)
+		db_key = str(db_obj.uniqueKey)
+		if db_key == req_uniqueKey:
+			if db_obj.key == req_otp:
+				if db_obj.status == 'PN':
+					db_obj.status = 'AC'
+					db_obj.save()
+					message = 'Thankyou Your account has been activated successufully! :)'
+				else:
+					message = 'Your account has already been activated'
+			else:
+				message = 'InValid Request!!'
 		else:
-			message = 'InValid Request!! :('
-	else:
-		message = 'InValid Request!! :('
+			message = 'Invalid Request!!!'
+			return HttpResponse(message)
+		return render(request, 'verify.html', {'message' : message})
+	else :
+		return HttpResponseRedirect('/signup/')
 
-	return render(request, 'verify.html', {'message' : message})
+def enterotp(request):
+	if request.method == 'GET':
+		email = request.GET.get('email','')
+		uniqueKey = request.GET.get('uniqueKey','')
+		context = {'email': email, 'uniqueKey': uniqueKey}
+		return render(request, 'enterotp.html', context)
+	else:
+		return HttpResponseRedirect('/signup/')
